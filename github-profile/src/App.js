7dcd15cf7d82e37797f039d9937fc4fd9ce1fa54
ahmedseragcodes <ahmedserag1@gmail.com>
2021-03-09 16:32:57 -0500

    have my profile loading successfully and componentDidMount working

--- github-profile/src/App.js
+++ github-profile/src/App.js
@@ -4,19 +4,35 @@ import React from "react";
 import "./index.css";
 //COMPONENT IMPORTS
 import ProfileCard from "./components/profileCard";
+import { BASE_URL, API_KEY } from "./constants/constants";
+import axios from "axios";
 
 class App extends React.Component {
   state={
-    myProfile: "",
+    myProfile: [],
     friends: [],
     signInForm: "",
+  }
+
+  //LOADS MY PROFILE INFORMATION UPON MOUNT
+  componentDidMount(){
+    axios.get(`${BASE_URL}/${API_KEY}`)
+      .then((res)=>{
+        console.log("APP COMP, SUCCEEDED GETTING MY PROFILE INFO", res);
+        this.setState({
+          ...this.state, myProfile: res.data
+        })
+      })
+      .catch((err)=>{
+        console.log("APP COMP, FAILED GETTING MY PROFILE INFO", err);
+      })
   }
 
   render(){
     return (
       <div className="catchAllContainer">
         <h1>@AhmedSeragCodes On Github</h1>
-        <ProfileCard />
+        <ProfileCard myProfile={this.state.myProfile} />
       </div>
     )
   }
---
+++ github-profile/src/constants/constants.js
@@ -0,0 +1,4 @@
+
+export const BASE_URL="https://api.github.com/users";
+export const API_KEY="ahmedseragcodes";
+
